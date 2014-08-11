dc6d9a618be31c37053438d9c97707fe7d4acf86
Lachlan Archibald <lachlan@lachytech.net>
2014-08-11 16:48:01 +1000

    Persistence + Entity Beans for User and Project Resources

--- src/main/java/net/sharemycode/Project.java
+++ src/main/java/net/sharemycode/Project.java
@@ -1,9 +1,95 @@
 package net.sharemycode;
 
 import java.util.Random;
+import java.io.Serializable;
 
-public class Project {
-	private String url;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@SuppressWarnings("serial")
+@Entity
+@XmlRootElement
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "url"))
+
+public class Project implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    @GeneratedValue
+    @Size(min=6)
+    private String url;
+    
+    @NotNull
+    @Size(min = 1, max = 20)
+    private String name;
+    
+    @NotNull
+    private long owner_id;
+
+    public Long getId() {
+        return id;
+    }
+
+    @NotNull
+    @Size(min=1)
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    @Size(max = 50)
+    private String description;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Long getOwner() {
+        return owner_id;
+    }
+
+    public void setOwner(Long owner_id) {
+        this.owner_id = owner_id;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String desc) {
+        this.description = desc;
+    }
+
+}
+
+/*
+private String url;
 	private String description;
 	private String path;
 	private String creator;
@@ -42,5 +128,4 @@ public class Project {
 		
 		public String getArchive() {
 			return this.archive;
-		}
-}
+		}*/
--- src/main/java/net/sharemycode/User.java
+++ src/main/java/net/sharemycode/User.java
@@ -1,15 +1,121 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.sharemycode;
 
-public class User {
-	
-	private int userID;
-	private String username;
-	private String password;
-	private String email;
-	private String givenName;
-	private String surname;
-	
-	
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import org.picketlink.idm.jpa.annotations.AttributeValue;
+import org.picketlink.idm.jpa.annotations.entity.IdentityManaged;
+
+
+@SuppressWarnings("serial")
+@Entity
+@XmlRootElement
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 25)
+    //@Pattern(regexp = "([a-zA-Z]|-)*", message = "Must not contain numbers")
+    @Pattern(regexp = "([a-zA-Z]|-)*", message = "Must only contain letters and '-'")
+    private String givenName;
+    
+    @NotNull
+    @Size(min = 1, max = 25)
+    @Pattern(regexp = "([a-zA-Z]|-)*", message = "Must only contain letters and '-'")
+    private String surname;
+
+    @NotNull
+    @Size(min=1)
+    @Email
+    private String email;
+
+    @NotNull
+    @Size(min = 8, max=26)
+    @Pattern(regexp = "", message = "Must contain a combination of digits, uppercase and lowercase letters and symbols ; : , @ & ! ^")
+    private String password;
+    
+    @NotNull
+    @Size(min = 10, max = 12)
+    @Digits(fraction = 0, integer = 12)
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String name) {
+        this.givenName = name;
+    }
+    
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String name) {
+        this.surname = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+/*
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+*/
+}
+/*
 	public User(String username, String password, String email, String given, String surname) {
 		this.username = username;
 		this.password = password;
@@ -17,22 +123,4 @@ public class User {
 		this.givenName = given;
 		this.surname = surname;
 		// TODO Add user to resource list
-	}
-	
-	// Accessors
-	public String getPassword() {
-		return this.password;
-	}
-	
-	public String getEmail() {
-		return this.email;
-	}
-	
-	public String getGivenName() {
-		return this.givenName;
-	}
-	
-	public String getSurname() {
-		return this.surname;
-	}
-}
+*/
