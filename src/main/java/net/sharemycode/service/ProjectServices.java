69127723218ab60c8b50095b4e56d6e71c449595
Lachlan Archibald <lachlan@lachytech.net>
2014-08-21 11:21:03 +1000

    Added ProjectAccess and Project_ metamodel class. Project is currently deploying on WildFly

--- src/main/java/net/sharemycode/service/ProjectServices.java
+++ src/main/java/net/sharemycode/service/ProjectServices.java
@@ -1,5 +1,6 @@
 package net.sharemycode.service;
 
+import javax.inject.Inject;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -10,6 +11,8 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.Consumes;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.Produces;
@@ -18,7 +21,10 @@ import javax.ws.rs.core.MediaType;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
+import net.sharemycode.service.controller.ProjectController;
 import net.sharemycode.service.model.Project;
+import net.sharemycode.service.model.ProjectResource;
+import net.sharemycode.service.model.ProjectResource.ResourceType;
 import net.sharemycode.service.model.User;
 
 /**
@@ -31,6 +37,9 @@ import net.sharemycode.service.model.User;
 
 @Path("/project")
 public class ProjectServices {
+	
+	@Inject ProjectController projectController;
+	
 	@GET
 	@Path("/list")
 	public String displayProjects() {
@@ -60,7 +69,81 @@ public class ProjectServices {
 			p.setName(projectName);
 			p.setVersion(version);
 			p.setDescription(description);
+			//projectController.createProject(p);
 			return Response.status(200).entity("{" + p.getName() + ", " + p.getVersion() + ", " + p.getDescription() + "}").build();
 
 	}
+	
+	 @GET
+	   @Path("/list{searchTerm:(/[^/]+?)?}")
+	   @Produces(MediaType.APPLICATION_JSON)
+	   public List<Project> listProjects(@PathParam("searchTerm") String searchTerm)
+	   {
+	      if (searchTerm.startsWith("/"))
+	      {
+	         searchTerm = searchTerm.substring(1);
+	      }
+	      return projectController.listProjects(searchTerm);
+	   }
+
+	   @POST
+	   @Path("/newfolder")
+	   @Consumes("application/json")
+	   @Produces(MediaType.APPLICATION_JSON)
+	   public ProjectResource[] createProjectFolder(Map<String,String> properties)
+	   {
+	      String[] parts = properties.get("name").split("/");
+	      List<ProjectResource> resources = new ArrayList<ProjectResource>();
+
+	      ProjectResource parent = null;
+
+	      if (properties.containsKey("parentResourceId"))
+	      {
+	         Long resourceId = Long.valueOf(properties.get("parentResourceId"));
+	         parent = projectController.lookupResource(resourceId);
+	      }
+
+	      for (String part : parts) 
+	      {
+	         ProjectResource r = new ProjectResource();
+	         r.setName(part);
+	         r.setResourceType(ResourceType.DIRECTORY);
+	         r.setParent(parent);
+
+	         if (properties.containsKey("projectId"))
+	         {
+	            Long projectId = Long.valueOf(properties.get("projectId"));
+	            r.setProject(projectController.lookupProject(projectId));
+	         }
+
+	         projectController.createResource(r);
+	         parent = r;
+	      }
+
+	      return resources.toArray(new ProjectResource[resources.size()]);
+	   }
+
+	   @POST
+	   @Path("/newclass")
+	   @Consumes("application/json")
+	   @Produces(MediaType.APPLICATION_JSON)
+	   public ProjectResource createProjectClass(Map<String,String> properties)
+	   {
+	      ProjectResource r = new ProjectResource();
+	      r.setName(properties.get("name") + ".java");
+	      r.setResourceType(ResourceType.FILE);
+
+	      Project p = projectController.lookupProject(Long.valueOf(properties.get("projectId")));
+	      r.setProject(p);
+
+	      String folder = properties.get("folder");
+	      String pkg = properties.get("package");
+
+	      ProjectResource parentDir = projectController.createDirStructure(p, folder);
+	      ProjectResource parentPkg = projectController.createPackage(p, parentDir, pkg);
+	      r.setParent(parentPkg);
+
+	      projectController.createResource(r);
+	      return r;
+	   }
 }
--- src/main/java/net/sharemycode/service/controller/ProjectController.java
+++ src/main/java/net/sharemycode/service/controller/ProjectController.java
@@ -1,5 +1,8 @@
 package net.sharemycode.service.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
 import javax.enterprise.event.Observes;
@@ -16,12 +19,15 @@ import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import net.sharemycode.service.events.NewProjectEvent;
+import net.sharemycode.service.events.NewResourceEvent;
 //import net.sharemycode.service.security.annotations.LoggedIn;
 import net.sharemycode.service.model.Project;
 import net.sharemycode.service.model.ProjectAccess;
 import net.sharemycode.service.model.ProjectAccess.AccessLevel;
 import net.sharemycode.service.model.ProjectResource;
 import net.sharemycode.service.model.ProjectResource.ResourceType;
+import net.sharemycode.service.model.Project_;
 import net.sharemycode.service.model.ResourceContent;
 
 import org.picketlink.Identity;
@@ -41,9 +47,9 @@ public class ProjectController
    @Inject
    private Instance<EntityManager> entityManager;
 
-   //@Inject Event<NewProjectEvent> newProjectEvent;
+   @Inject Event<NewProjectEvent> newProjectEvent;
 
-  // @Inject Event<NewResourceEvent> newResourceEvent;
+   @Inject Event<NewResourceEvent> newResourceEvent;
 
    @Inject
    private Identity identity;
@@ -62,7 +68,7 @@ public class ProjectController
 
       em.persist(pa);
 
-    //  newProjectEvent.fire(new NewProjectEvent(project));
+      newProjectEvent.fire(new NewProjectEvent(project));
    }
 
  //  @LoggedIn
@@ -77,7 +83,7 @@ public class ProjectController
 
       em.persist(content);
 
-    //  newResourceEvent.fire(new NewResourceEvent(resource));
+      newResourceEvent.fire(new NewResourceEvent(resource));
    }
 
    public Project lookupProject(Long id)
@@ -90,6 +96,103 @@ public class ProjectController
    {
       EntityManager em = entityManager.get();
       return em.find(ProjectResource.class, id);
+   }
+   
+   public ProjectResource lookupResourceByName(Project project, ProjectResource parent, String name)
+   {
+      EntityManager em = entityManager.get();
+
+      TypedQuery<ProjectResource> q = em.createQuery(
+               "select r from ProjectResource r where r.project = :project and r.parent = :parent and r.name = :name",
+               ProjectResource.class);
+      q.setParameter("project", project);
+      q.setParameter("parent", parent);
+      q.setParameter("name", name);
+
+      try
+      {
+        return q.getSingleResult();
+      }
+      catch (NoResultException ex)
+      {
+         return null;
+      }
+   }
+   
+   public List<Project> listProjects(String searchTerm)
+   {
+      EntityManager em = entityManager.get();
+
+      CriteriaBuilder cb = em.getCriteriaBuilder();
+      CriteriaQuery<ProjectAccess> cq = cb.createQuery(ProjectAccess.class);
+      Root<ProjectAccess> from = cq.from(ProjectAccess.class);
+      Join<ProjectAccess,Project> project = from.join("project");
+
+      List<Predicate> predicates = new ArrayList<Predicate>();
+
+      predicates.add(cb.equal(from.get("userId"), identity.getAccount().getId()));
+
+      if (searchTerm != null && !"".equals(searchTerm))
+      {
+         predicates.add(cb.like(cb.lower(project.get(Project_.name)), "%" + searchTerm.toLowerCase() + "%"));
+      }
+
+      cq.where(predicates.toArray(new Predicate[predicates.size()]));
+
+      TypedQuery<ProjectAccess> q = em.createQuery(cq);
+
+      List<Project> projects = new ArrayList<Project>();
+      for (ProjectAccess a : q.getResultList())
+      {
+         projects.add(a.getProject());
+      }
+
+      return projects;
+   }
+
+   public List<ProjectResource> listResources(Project project)
+   {
+      TypedQuery<ProjectResource> q = entityManager.get().<ProjectResource>createQuery("select r from ProjectResource r where r.project = :project", 
+               ProjectResource.class);
+      q.setParameter("project", project);
+      return q.getResultList();
+   }
+
+   public ProjectResource createDirStructure(Project project, String directory)
+   {
+      String[] parts = directory.split(ProjectResource.PATH_SEPARATOR);
+
+      ProjectResource parent = null;
+      for (int i = 0; i < parts.length; i++)
+      {
+         // If the first part is equal to the project name, ignore it
+         if (i == 0 && parts[i].equals(project.getName()))
+         {
+            continue;
+         }
+
+         ProjectResource r = lookupResourceByName(project, parent, parts[i]);
+         // If the directory resource doesn't exist, create it
+         if (r == null)
+         {
+            r = new ProjectResource();
+            r.setProject(project);
+            r.setResourceType(ResourceType.DIRECTORY);
+            r.setName(parts[i]);
+            r.setParent(parent);
+            createResource(r);
+         }
+
+         parent = r;
+      }
+
+      return parent;
+   }
+
+   public ProjectResource createPackage(Project project, ProjectResource folder, String pkgName)
+   {
+
+      return null;
    }
 }
 
