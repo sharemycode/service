076eafaf4d33caa1c8fdd08c658880822071acda
Lachlan Archibald <lachlan@lachytech.net>
2014-08-25 16:54:23 +1000

    Replaced Service.java with SystemServices.java, added /client/test GET request

---
+++ src/main/java/net/sharemycode/service/SystemServices.java
@@ -0,0 +1,32 @@
+package net.sharemycode.service;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ * sharemycode RESTEasy Web SystemServices
+ *
+ */
+
+@Path("")
+public class SystemServices {
+	/*@GET
+	@Path("/{pathParameter}")
+	public Response responseMsg( @PathParam("pathParameter") String pathParameter,
+    		@DefaultValue("No response") @QueryParam("q") String queryParameter) {
+		
+		String response = "Hello from: " + pathParameter + ": " + queryParameter;
+		return Response.status(200).entity(response).build();
+	}*/
+	
+	@GET
+	@Path("/client/test")
+	public Response clientTest() {
+		String response = "Hello client! Connection successful!";
+		return Response.status(200).entity(response).build();
+	}
+	
+}
